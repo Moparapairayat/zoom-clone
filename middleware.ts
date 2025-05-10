01fe5b5c5223767b45a5df15598aa50523cc4094
MOPARA PAIR AYAT <83800319+Moparapairayat@users.noreply.github.com>
2025-05-10 10:43:22 +0600

    Fix Clerk authentication implementation

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const protectedRoute = createRouteMatcher([
-  '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-  '/personal-room',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
+import { authMiddleware } from '@clerk/nextjs';
+ 
+export default authMiddleware({
+  publicRoutes: ['/sign-in(.*)', '/sign-up(.*)'],
 });
-
+ 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
