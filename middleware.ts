8a9d75c2c13a33ec99bf5e4a187498773d92351f
MOPARA PAIR AYAT <83800319+Moparapairayat@users.noreply.github.com>
2025-05-10 10:45:13 +0600

    Fix Clerk middleware import and usage

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { withClerkMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-export default authMiddleware({
+export default withClerkMiddleware({
   afterAuth: () => {
     return NextResponse.next();
   }
